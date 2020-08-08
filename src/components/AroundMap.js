3bc3b12f31309d8fe845f40f2a60b7a3c6c4104e
yuezhou <yuezh203@gmail.com>
2020-08-08 01:17:49 -0700

    init commit

---
+++ src/components/AroundMap.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import { POS_KEY } from '../constants';
+
+import {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+} from "react-google-maps";
+
+class NormalAroundMap extends Component {
+    getMapRef = (mapInstance) => {
+        this.map = mapInstance;
+        window.map = mapInstance;
+    }
+
+    reloadMarker = () => {
+        const center = this.getCenter();
+        const radius = this.getRadius();
+        this.props.loadPostsByTopic(center, radius);
+    }
+
+    getCenter() {
+        const center = this.map.getCenter();
+        return { lat: center.lat(), lon: center.lng() };
+    }
+
+    getRadius() {
+        const center = this.map.getCenter();
+        const bounds = this.map.getBounds();
+        if (center && bounds) {
+            const ne = bounds.getNorthEast();
+            const right = new window.google.maps.LatLng(center.lat(), ne.lng());
+            return 0.001 * window.google.maps.geometry.spherical.computeDistanceBetween(center, right);
+        }
+    }
+
+    render() {
+        const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
+        return (
+            <GoogleMap
+                ref={this.getMapRef}
+                defaultZoom={11}
+                defaultCenter={{ lat, lng: lon }}
+                onDragEnd={this.reloadMarker}
+                onZoomChanged={this.reloadMarker}
+            >
+            </GoogleMap>
+        );
+    }
+}
+
+const AroundMap = withScriptjs(withGoogleMap(NormalAroundMap));
+
+export default AroundMap;
+
